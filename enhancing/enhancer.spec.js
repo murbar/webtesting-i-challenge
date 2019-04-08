66aec97103da05a4866818a86c49b93913d656fc
Joel Bartlett <joel@murbar.com>
2019-04-08 17:14:42 -0400

    Test and implement repair

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,10 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('repair', () => {
+  it('should restore item durability to 100', () => {
+    const { durability } = enhancer.repair({});
+    expect(durability).toBe(100);
+  });
+});
+
